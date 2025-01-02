0c6726486032434ea6876ec82e10f36dcbc1decb
Szymon Bryniak <105818379+SzymonBryniak@users.noreply.github.com>
2025-01-02 22:00:48 +0000

    Day 53 progress

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ response = requests.get("https://appbrewery.github.io/Zillow-Clone/").text
 # print(response.text)
 soup = BeautifulSoup(response, 'html.parser')
 
-for i in soup.find_all('a', class_="StyledPropertyCardDataArea-anchor"):
-  print(i.get('href'))
+links = [i.get('href') for i in soup.find_all('a', class_="StyledPropertyCardDataArea-anchor")]
+prices = [re.sub("[+/mo 1bd]","", i.text) for i in soup.find_all('span', class_="PropertyCardWrapper__StyledPriceLine")]
+print(prices)
 
-for i in soup.find_all('span', class_="PropertyCardWrapper__StyledPriceLine"):
-  print(re.sub("[+/mo 1bd]","", i.text))
+
 # https://docs.google.com/forms/d/e/1FAIpQLSeb87optrzogoT11OxsFNh5K5WrVZzV_rrvUreKxQrcCHZCxg/viewform?usp=header

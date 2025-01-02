aefb02cf1e9a324263e396bea19f4aa3b9724863
Szymon Bryniak <szymonbryniakproject@gmail.com>
2025-01-02 20:59:24 +0000

    Day 53 progress

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 response = requests.get("https://appbrewery.github.io/Zillow-Clone/").text
 # print(response.text)
 soup = BeautifulSoup(response, 'html.parser')
 
-for i in soup.find_all('li', class_="ListItem-c11n-8-84-3-StyledListCardWrapper"):
-  # print(i)2
-  print(soup.find('a', class_="StyledPropertyCardDataArea-anchor"))
+for i in soup.find_all('a', class_="StyledPropertyCardDataArea-anchor"):
+  print(i.get('href'))
+
+for i in soup.find_all('span', class_="PropertyCardWrapper__StyledPriceLine"):
+  print(re.sub("[+/mo 1bd]","", i.text))
 # https://docs.google.com/forms/d/e/1FAIpQLSeb87optrzogoT11OxsFNh5K5WrVZzV_rrvUreKxQrcCHZCxg/viewform?usp=header
